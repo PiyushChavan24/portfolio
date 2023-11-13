1984d3ac8fd3cad2e23c28e0c871be2a47a8003e
piyushchavan24 <piyushchavan24@gmail.com>
2023-11-13 19:49:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./components/Navbar/Navbar";
+import Intro from"./components/intro/intro"
 function App() {
   return (
     <div className="App"> 
       <Navbar/>
+      <Intro/>
     </div>
   );
 }
